671b9145f60354f1558bf21c06c486e7368312ed
TrishnaChakraborty <1305089.tc@ugrad.cse.buet.ac.bd>
2015-09-19 21:09:30 +0600

    This is my first commit

---
+++ src/Circle.java
@@ -0,0 +1,23 @@
+/**
+ * Created by DELL on 9/19/2015.
+ */
+public class Circle {
+    private double radius;
+    private Point center;
+    private String color;
+
+    public Circle(Point center, double radius, String color) {
+        this.center = center;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public Point getCenter() {
+        return this.center;
+    }
+
+    public void print() {
+        System.out.println("Center: (" + getCenter().getX() + "," + getCenter().getY() +
+                ") Radius: " + radius + " Color: "  + color);
+    }
+}
